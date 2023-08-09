a62302aa797b2eecde95ba36347ab3e7c7d8ea87
Eduard Miterev <nichegohair@gmail.com>
2023-08-09 18:38:47 +0400

    add theme toggler

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { MainPageLazy } from "../pages/MainPage/MainPage.lazy";
 import { AboutPageLazy } from "../pages/AboutPage/AboutPage.lazy";
-import "./index.scss";
+import "../styles/index.scss";
+import { useTheme } from "../theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная страница</Link> <br />
       <Link to={"/about"}>Страница о сайте</Link>
-      <Suspense fallback={<h1>Loading...</h1>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/"} element={<MainPageLazy />} />
           <Route path={"/about"} element={<AboutPageLazy />} />
