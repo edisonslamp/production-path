14a24ed07fb4e4688678b1523dc092661fa3af9e
Eduard Miterev <nichegohair@gmail.com>
2023-08-09 16:27:30 +0400

    begin styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from "./Counter";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainPageLazy } from "../pages/MainPage/MainPage.lazy";
+import { AboutPageLazy } from "../pages/AboutPage/AboutPage.lazy";
 import "./index.scss";
 
 export const App = () => {
   return (
-    <div className="app">
-      Hello World!
-      <Counter />
+    <div>
+      <Link to={"/"}>Главная страница</Link> <br />
+      <Link to={"/about"}>Страница о сайте</Link>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageLazy />} />
+          <Route path={"/about"} element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
