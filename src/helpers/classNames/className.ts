8e0d14b118be4cb300c5a829578ab6f473eb8db5
Eduard Miterev <nichegohair@gmail.com>
2023-08-10 19:02:38 +0400

    add className function

---
+++ src/helpers/classNames/className.ts
@@ -0,0 +1,19 @@
+type Mods = Record<string, boolean | string>;
+
+export default function className(
+  cls: string,
+  mods?: Mods,
+  additional?: string[]
+): string {
+  const mod: string[] = [];
+  Object.entries(mods).forEach(([key, value]) => {
+    if (value) {
+      mod.push(key);
+    }
+  });
+
+  return [cls, ...additional, ...mod].join(" ");
+}
+
+// Пример как должна вызываться подобная функция
+// className("app", { hovered: true, selected: false }, ["something additional"]); => 'add hovered something additional'
