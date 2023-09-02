230ee24b7744864f0d8d3c352f8459ea8ca91894
Eduard Miterev <nichegohair@gmail.com>
2023-09-02 17:09:56 +0300

    add first tests with jest

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,40 @@
 import { classNames } from "./classNames";
 
 describe("classNames", () => {
-    expect(classNames("class")).toBe("class");
+    test("with only one first class", () => {
+        expect(classNames("someClass")).toBe("someClass");
+    });
+
+    test("with class and additional classes", () => {
+        expect(classNames("someClass", {}, ["addCls1", "addCls2"])).toBe(
+            "someClass addCls1 addCls2",
+        );
+    });
+
+    test("with class, mods and additional classes", () => {
+        expect(
+            classNames("someClass", { hovered: true, scrollable: true }, [
+                "addCls1",
+                "addCls2",
+            ]),
+        ).toBe("someClass addCls1 addCls2 hovered scrollable");
+    });
+
+    test("with class, additional classes and two modes with one of them false", () => {
+        expect(
+            classNames("someClass", { hovered: true, scrollable: false }, [
+                "addCls1",
+                "addCls2",
+            ]),
+        ).toBe("someClass addCls1 addCls2 hovered");
+    });
+
+    test("with class, additional classes and two modes with one of them undefined", () => {
+        expect(
+            classNames("someClass", { hovered: true, scrollable: undefined }, [
+                "addCls1",
+                "addCls2",
+            ]),
+        ).toBe("someClass addCls1 addCls2 hovered");
+    });
 });
