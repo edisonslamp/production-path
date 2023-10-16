3ba94afce998cecfae46df7089d3791766533f49
Eduard Miterev <nichegohair@gmail.com>
2023-10-16 14:05:18 +0200

    fix translation literals

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "shared/ui";
 import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
@@ -6,6 +7,7 @@ import { counterActions } from "../model/slice/counterSlice";
 export const Counter = () => {
     // чтобы использовать редусеры мы должны передавать их через диспатч!
     const dispatch = useDispatch();
+    const { t } = useTranslation();
 
     // чтобы достать данные из стейта нужно использовать селектор useSelector()
     // также селекторы нужно выносить в отдельный модуль, а не указывать их в компоненте напрямую
@@ -21,12 +23,15 @@ export const Counter = () => {
 
     return (
         <div>
-            <h1 data-testid="value-title">Value:{value}</h1>
+            <h1 data-testid="value-title">
+                {t("Value:")}
+                {value}
+            </h1>
             <Button onClick={increment} data-testid="increment-btn">
-                increment
+                {t("increment")}
             </Button>
             <Button onClick={decrement} data-testid="decrement-btn">
-                decrement
+                {t("decrement")}
             </Button>
         </div>
     );
